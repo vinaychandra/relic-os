f3960beadb347e9233e6666ed99f6c3ab875236e
Vinay Chandra Dommeti <github@vinay.vc>
2021-04-12 12:10:16 -0700

    CAddr support

--- crates/common/relic-abi/src/lib.rs
+++ crates/common/relic-abi/src/lib.rs
@@ -1,1 +1,14 @@
 #![cfg_attr(not(test), no_std)]
+
+pub mod caddr;
+
+/// Prelude to re-upload commonly used items.
+pub mod prelude {
+    pub use crate::caddr::CAddr;
+}
+
+/// A trait that allows resetting a struct back to its default value.
+pub trait SetDefault {
+    /// Reset this struct back to its default value.
+    fn set_default(&mut self);
+}
