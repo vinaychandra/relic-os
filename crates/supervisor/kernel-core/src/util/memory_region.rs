77ffe4fa25a479a1d87ae1ec42ce7ae282dfd4c8
Vinay Chandra Dommeti <github@vinay.vc>
2021-04-12 11:27:41 -0700

    Memory region struct

---
+++ crates/supervisor/kernel-core/src/util/memory_region.rs
@@ -0,0 +1,62 @@
+use crate::addr::PAddr;
+
+/// Represents a memory region with a start physical address and a
+/// length.
+#[derive(Debug, Copy, Clone)]
+pub struct MemoryRegion {
+    start_paddr: PAddr,
+    length: usize,
+}
+
+impl MemoryRegion {
+    /// Start address of the memory region.
+    pub fn start_paddr(&self) -> PAddr {
+        self.start_paddr
+    }
+
+    /// Test whether the provided address is contained in this memory region or not.
+    pub fn contains(&self, other: PAddr) -> bool {
+        self.start_paddr <= other && self.end_paddr() >= other
+    }
+
+    /// Length of the memory region.
+    pub fn length(&self) -> usize {
+        self.length
+    }
+
+    /// End address of the memory region. (Inclusive)
+    pub fn end_paddr(&self) -> PAddr {
+        self.start_paddr + (self.length - 1)
+    }
+
+    /// Modify the current memory region so that it skip up to the
+    /// argument `region`.
+    pub fn skip_up(&mut self, region: &MemoryRegion) -> bool {
+        if self.start_paddr() <= region.start_paddr() && self.end_paddr() >= region.end_paddr() {
+            self.move_up(region.start_paddr() + region.length());
+
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Modify the current memory region so that it move to the
+    /// beginning of `npaddr`.
+    pub fn move_up(&mut self, npaddr: PAddr) {
+        assert!(npaddr >= self.start_paddr);
+        assert!(self.start_paddr + self.length > npaddr);
+        let nlength =
+            Into::<usize>::into(self.start_paddr + self.length) - Into::<usize>::into(npaddr);
+        self.length = nlength;
+        self.start_paddr = npaddr;
+    }
+
+    /// Create a new memory region using `start_paddr` and `length`.
+    pub fn new(start_paddr: PAddr, length: usize) -> MemoryRegion {
+        MemoryRegion {
+            start_paddr,
+            length,
+        }
+    }
+}
--- crates/supervisor/kernel-core/src/util/mod.rs
+++ crates/supervisor/kernel-core/src/util/mod.rs
@@ -1,2 +1,3 @@
 pub mod managed_arc;
 pub mod memory_object;
+pub mod memory_region;
